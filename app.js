4239ef2186d7fc60cf4c240c574bc20d38724039
Meg <ai285063@gmail.com>
2021-06-14 15:52:44 +0800

    Add restaurant index page

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
-// require('./routes')(app)
+require('./routes')(app)
 
 module.exports = app
